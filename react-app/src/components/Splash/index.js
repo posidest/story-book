9c5ed5c0d09c457e2aa5862309bee4c84f1d0b78
Alana <alana.lapoint@gmail.com>
2021-06-22 17:53:46 -0400

    basic sitewide preliminary styles

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
 import {Redirect} from 'react-router-dom'
+import './Splash.css'
 
 const Splash = () => {
    const me = useSelector(state => state.session.user)
@@ -11,7 +12,7 @@ const Splash = () => {
 
 
    return (
-      <div>
+      <div className='splash'>
          <h1>Story Book</h1>
          <h4>What's the story, morning glory?</h4>
       </div>
