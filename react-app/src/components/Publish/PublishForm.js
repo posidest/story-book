15270769d5b5717eac0cacf43683bef945b308f6
Alana <alana.lapoint@gmail.com>
2021-07-30 16:53:55 -0400

    played with editing

--- react-app/src/components/Publish/PublishForm.js
+++ react-app/src/components/Publish/PublishForm.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {useSelector} from 'react-redux';
+import React, {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import './Publish.css'
 
@@ -7,19 +7,61 @@ const PublishForm = (tempId) => {
    const me = useSelector(state => state.session.user)
    const pages = useSelector(state => state.page.pages)
    const book = useSelector(state => state.book)
-   console.log(book, 'book from publish form page')
-   console.log(pages, 'pages from publish form page')
+   const dispatch = useDispatch()
+   // console.log(book, 'book from publish form page')
+   // console.log(pages, 'pages from publish form page')
+   const [title, setTitle] = useState(book.book.title)
+   const [image, setImage] = useState(book.book.image)
+   const [leaves, setLeaves] = useState([])
+   const [description, setDescription] = useState(book.book.description)
+   const hidden = 'hidden'
+   const inputClasses = ['titleInput', 'coverInput', 'descriptionInput']
+   // const [title, setTitle] = useState(book.book.title)
+   // const [title, setTitle] = useState(book.book.title)
+   
    if (!me) return ( 
-   <Redirect to='/'/>
-   )
+      <Redirect to='/'/>
+      )
+      
+   // useEffect(() => {
+   //    if(pages) {
+   //       setLeaves(pages)
+   //    }
+   // },[pages])
+
+   const updateTitle = (e) => {
+      setTitle(e.target.value)
+   }
+
+   const showField = (e) => {
+      // const field = document.querySelectorAll(`.${className}`)
+      // const field = e.target.closest('input')
+      const field = document.querySelectorAll(`.${inputClasses[0]}`)[0]
+      console.log(field)
+      field.classList.contains(hidden) ?
+      field.classList.remove(hidden) :
+      field.classList.add(hidden)
+   }
+
+   const publishBook = (e) => {
+
+   }
 
    if (pages && book) {
       return (
          <div className='publish'>
-            <div className='cover'>
-               <h1>{book.book.title}</h1>
-               <img src={book.book.image} alt='book-cover'/>
-               <h4>{book.book.description}</h4>
+            <form onSubmit={publishBook}>
+               <div className='cover'>
+                  <h1>{book.book.title}</h1>
+                  <input 
+                  type='text'
+                  className={`${hidden} ${inputClasses[0]}`}
+                  value={title}
+                  onChange={updateTitle}
+                  />
+                  <button type='button' className='edit-btn' onClick={showField}>edit</button> 
+                  <img src={book.book.image} alt='book-cover'/>
+                  <h4>{book.book.description}</h4>
             </div>
             <div className='pages'>
                {pages.map((page) => (
@@ -33,6 +75,8 @@ const PublishForm = (tempId) => {
                   </>
                ))}
             </div>
+
+            </form>
          </div>
       )
    } else {
